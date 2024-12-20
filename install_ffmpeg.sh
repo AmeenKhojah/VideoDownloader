40d853da8192bdd6f98b99b8f2f5a52c101f0c1e
Ameen Khojah <amin.n.khojah@gmail.com>
2024-12-21 02:29:41 +0300

    Delete install_ffmpeg.sh

--- install_ffmpeg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting FFmpeg installation..." > /app/ffmpeg_debug.log
-chmod +x ./install_ffmpeg.sh >> /app/ffmpeg_debug.log 2>&1
-apt-get update >> /app/ffmpeg_debug.log 2>&1
-apt-get install -y ffmpeg >> /app/ffmpeg_debug.log 2>&1
-echo "FFmpeg installation complete!" >> /app/ffmpeg_debug.log
